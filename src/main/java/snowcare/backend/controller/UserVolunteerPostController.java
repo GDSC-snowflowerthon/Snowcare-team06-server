619ab8296d204bef4320989fe2cf70f3fc7834b3
alsrudursla <nuy0307@naver.com>
2024-01-11 20:30:07 +0900

    chore : UserVolunteerPostController 어노테이션 변경

--- src/main/java/snowcare/backend/controller/UserVolunteerPostController.java
+++ src/main/java/snowcare/backend/controller/UserVolunteerPostController.java
@@ -5,9 +5,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import snowcare.backend.service.UserVolunteerPostService;
 
-@Service
+@RestController
 @RequiredArgsConstructor
 @Slf4j
 @RequestMapping("/users/record")
